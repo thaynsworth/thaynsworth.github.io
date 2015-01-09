97f8325ac5638476d3a89e16b44b146d75ea1d55
thaynsworth <thomas.haynsworth@gmail.com>
2015-01-09 16:05:01 -0500

    updated verbage

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,13 +1,5 @@
 console.log('app.js works!')
  
- $(function(){
-
-    $('div.walksafe').hover(
-      function() {
-        $(this).animate({'background-color':'blue'});
-      },
-      function(){
-        $(this).animate({opacity:'0'});
-      }
-    ); 
-  }) 
+$(function(){
+	
+}) 
