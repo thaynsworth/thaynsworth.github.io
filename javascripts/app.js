87d3abd5800762cb67bf799c90a6cbcc8152031a
thaynsworth <thomas.haynsworth@gmail.com>
2015-01-09 15:49:57 -0500

    added hover/description to projects

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,8 +1,13 @@
 console.log('app.js works!')
+ 
+ $(function(){
 
-
-$(function() {
-
-
-
-})
+    $('div.walksafe').hover(
+      function() {
+        $(this).animate({'background-color':'blue'});
+      },
+      function(){
+        $(this).animate({opacity:'0'});
+      }
+    ); 
+  }) 
