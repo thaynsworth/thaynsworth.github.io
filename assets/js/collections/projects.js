71c6758273f31427e2fe903eab575f3c89c30f45
thaynsworth <thomas.haynsworth@gmail.com>
2015-06-02 22:10:00 -0400

    refactored utilizing backbone

--- assets/js/collections/projects.js
+++ assets/js/collections/projects.js
@@ -10,18 +10,18 @@ var apps = [
 	},		
 	{
 		name: 'Walksafe',
-		url: 'assets/img/walksafe1.png',
+		url: 'assets/img/walksafe2.png',
 		description: ' provides a visualization of crime within the 5 boroughs of New York City. Users can create an account, then use the map to inform themselves of the safest streets or neighborhoods.'
 	},
 	{
 		name: 'Project Share',
-		url: 'assets/img/projectshare1.png',
-		description: ' was a group collaboration with three other classmates. It is a place for creators and professionals to start a project of their own and/or join other users on their project. Users can follow, comment and collaborate on other projects. <button class="btn btn-primary" href="http://104.131.217.121">Click here!</button>'
+		url: 'assets/img/projectshare3.png',
+		description: ' was a group collaboration with three other classmates. It is a place for creators and professionals to start a project of their own and/or join other users on their project. Users can follow, comment and collaborate on other projects.'
 	},	
 	{
 		name: 'Joan Haynsworth Art',
 		url: 'assets/img/joan-haynsworth-art.png',
-		description: ' is where Joan haynsworth sells and displays all of her art creations. <button class="btn btn-primary" href="https://joan-haynsworth-art.herokuapp.com/">Click here!</button>'
+		description: ' is where Joan haynsworth sells and displays all of her art creations.'
 	}
 
 ];
