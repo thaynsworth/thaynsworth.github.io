6188a990063c1088034808e47d97f75cba27c375
thaynsworth <thomas.haynsworth@gmail.com>
2015-06-02 23:47:53 -0400

    renamed css files removed underline

--- assets/js/collections/projects.js
+++ assets/js/collections/projects.js
@@ -7,26 +7,26 @@ var apps = [
 	{
 		name: 'WalkSafe',
 		url: 'assets/img/walksafe2.png',
-		description: 'WalkSafe provides a visualization of crime within the 5 boroughs of New York City. Users can create an account, then use the map to inform themselves of the safest streets or neighborhoods.',
-		stack: 'Rails/PostgreSQL/GeoJSON/MapBox/Javascript/HTML5/CSS3'
+		description: 'WalkSafe provides a visualization of crime within the 5 boroughs of New York City. Users can create an account, then use the map to inform themselves of the safest streets or neighborhoods.\n',
+		stack: '\nRails/PostgreSQL/GeoJSON/MapBox/Javascript/HTML5/CSS3'
 	},
 	{
 		name: 'ProjectShare',
 		url: 'assets/img/projectshare3.png',
 		description: 'ProjectShare was a group collaboration with three other developers. It is a place for creators and professionals to start a project of their own and/or join other users on their project. Users can follow, comment, and collaborate on other projects.',
-		stack: 'Rails/PostgreSQL/Bootstrap/HTML5/CSS3'
+		stack: '\nRails/PostgreSQL/Bootstrap/HTML5/CSS3'
 	},	
 	{
 		name: 'Joan Haynsworth Art',
 		url: 'assets/img/joan-haynsworth-art.png',
 		description: 'Joan Haynsworth\'s professional art gallery, consisting of oil, pastel, and digital creations.',
-		stack: 'Rails/PostgreSQL/Javascript/HTML5/CSS3/Paperclip/Skeleton'
+		stack: '\nRails/PostgreSQL/Javascript/HTML5/CSS3/Paperclip/Skeleton'
 	},
 	{
 		name: 'The Erik Hendricks Memorial Foundation',
 		url: 'assets/img/coming_soon2.png',
 		description: 'The Erik Hendricks Memorial Foundation raises money for Hairy Cell Leukemia.',
-		stack: 'Rails/PostgreSQL/Bootstrap/HTML5/CSS3'
+		stack: '\nRails/PostgreSQL/Bootstrap/HTML5/CSS3'
 	}
 
 ];
