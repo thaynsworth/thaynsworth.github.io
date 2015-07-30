124a217032eaca1fc124945c4941c616b49ef7c8
thaynsworth <thomas.haynsworth@gmail.com>
2015-07-30 16:04:42 -0400

    added devicons .each to html, works

--- assets/js/collections/projects.js
+++ assets/js/collections/projects.js
@@ -8,35 +8,35 @@ var apps = [
 		name: 'WalkSafe',
 		url: 'assets/img/walksafe2.png',
 		description: 'WalkSafe provides a visualization of crime within the 5 boroughs of New York City. Users can create an account, then use the map to inform themselves of the safest streets or neighborhoods.\n',
-		stack: '\nRails/PostgreSQL/GeoJSON/MapBox/Javascript/HTML5/CSS3', 
+		stack: ["devicon-html5-plain colored", "devicon-css3-plain colored"], 
 		target: 'walksafe-target'
 	},
 	{
 		name: 'ProjectShare',
 		url: 'assets/img/projectshare3.png',
 		description: 'ProjectShare was a group collaboration with three other developers. It is a place for creators and professionals to start a project of their own and/or join other users on their project. Users can follow, comment, and collaborate on other projects.',
-		stack: 'This is a test',
+		stack: ["devicon-rails-plain colored", "devicon-html5-plain colored", "devicon-css3-plain colored"],
 		target: 'projectshare-target'
 	},	
 	{
 		name: 'Joan Haynsworth Art',
 		url: 'assets/img/joan-haynsworth-art.png',
 		description: 'Joan Haynsworth\'s professional art gallery, consisting of oil, pastel, and digital creations.',
-		stack: '\nRails/PostgreSQL/Javascript/HTML5/CSS3/Paperclip/Skeleton',
+		stack: ["devicon-rails-plain colored", "devicon-html5-plain colored", "devicon-css3-plain colored"],
 		target: 'joan-target'
 	},
 	{
 		name: 'The Erik Hendricks Memorial Foundation',
 		url: 'assets/img/ehmf.png',
 		description: 'The Erik Hendricks Memorial Foundation raises money for Hairy Cell Leukemia.',
-		stack: '\nRails/PostgreSQL/Bootstrap/HTML5/CSS3',
+		stack: ["devicon-rails-plain colored", "devicon-html5-plain colored", "devicon-css3-plain colored"],
 		target: 'ehmf-target'
 	},
 	{
 		name: 'MTI Integrated Business Development Inc.',
 		url: 'assets/img/coming_soon2.png',
 		description: 'A vocational rehabilitation services provider, serving more than 100 Veterans annually.',
-		stack: '\nWordPress/PHP/HTML5/CSS3',
+		stack: ["devicon-rails-plain colored", "devicon-html5-plain colored", "devicon-css3-plain colored"],
 		target: 'mti-target'
 	}
 
