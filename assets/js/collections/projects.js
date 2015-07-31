2efd43f27aef75149fc162fac9be19b01ee71156
thaynsworth <thomas.haynsworth@gmail.com>
2015-07-30 21:47:40 -0400

    updated tech icon data

--- assets/js/collections/projects.js
+++ assets/js/collections/projects.js
@@ -8,7 +8,7 @@ var apps = [
 		name: 'WalkSafe',
 		url: 'assets/img/walksafe2.png',
 		description: 'WalkSafe provides a visualization of crime within the 5 boroughs of New York City. Users can create an account, then use the map to inform themselves of the safest streets or neighborhoods.\n',
-		stack: ["devicon-ruby-plain colored", "devicon-javascript-plain colored", "devicon-jquery-plain colored", "devicon-html5-plain colored", "devicon-css3-plain colored"], 
+		stack: ["devicon-ruby-plain-wordmark colored", "devicon-javascript-plain colored", "devicon-jquery-plain-wordmark colored", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored", "devicon-postgresql-plain-wordmark colored"], 
 		target: 'walksafe-target',
 		site: null,
 		github: 'https://github.com/thaynsworth/final_project'
@@ -17,7 +17,7 @@ var apps = [
 		name: 'ProjectShare',
 		url: 'assets/img/projectshare3.png',
 		description: 'ProjectShare was a group collaboration with three other developers. It is a place for creators and professionals to start a project of their own and/or join other users on their project. Users can follow, comment, and collaborate on other projects.',
-		stack: ["devicon-rails-plain colored", "devicon-html5-plain colored", "devicon-css3-plain colored"],
+		stack: ["devicon-ruby-plain-wordmark colored", "devicon-rails-plain-wordmark colored", "devicon-javascript-plain colored", "devicon-jquery-plain-wordmark colored", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored", "devicon-postgresql-plain-wordmark colored", "devicon-bootstrap-plain-wordmark colored"],
 		target: 'projectshare-target',
 		site: 'http://104.131.217.121/',
 		github: 'https://github.com/thaynsworth/Gnarwhales'
@@ -26,7 +26,7 @@ var apps = [
 		name: 'Joan Haynsworth Art',
 		url: 'assets/img/joan-haynsworth-art.png',
 		description: 'Joan Haynsworth\'s professional art gallery, consisting of oil, pastel, and digital creations.',
-		stack: ["devicon-rails-plain colored", "devicon-html5-plain colored", "devicon-css3-plain colored"],
+		stack: ["devicon-ruby-plain-wordmark colored", "devicon-rails-plain-wordmark colored", "devicon-javascript-plain colored", "devicon-jquery-plain-wordmark colored", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored", "devicon-postgresql-plain-wordmark colored"],
 		target: 'joan-target',
 		site: 'http://www.joanhaynsworthart.com',
 		github: 'https://github.com/thaynsworth/JoanOfArt'
@@ -35,7 +35,7 @@ var apps = [
 		name: 'The Erik Hendricks Memorial Foundation',
 		url: 'assets/img/ehmf.png',
 		description: 'The Erik Hendricks Memorial Foundation raises money for Hairy Cell Leukemia.',
-		stack: ["devicon-rails-plain colored", "devicon-html5-plain colored", "devicon-css3-plain colored"],
+		stack: ["devicon-ruby-plain-wordmark colored", "devicon-rails-plain-wordmark colored", "devicon-javascript-plain colored", "devicon-jquery-plain-wordmark colored", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored", "devicon-postgresql-plain-wordmark colored", "devicon-bootstrap-plain-wordmark colored"],
 		target: 'ehmf-target',
 		site: 'http://www.theerikhendricksmemorialfoundation.com',
 		github: 'https://github.com/thaynsworth/erik-hendricks-foundation'
@@ -44,7 +44,7 @@ var apps = [
 		name: 'MTI Integrated Business Development Inc.',
 		url: 'assets/img/coming_soon2.png',
 		description: 'A vocational rehabilitation services provider, serving more than 100 Veterans annually.',
-		stack: ["devicon-rails-plain colored", "devicon-html5-plain colored", "devicon-css3-plain colored"],
+		stack: ["devicon-wordpress-plain-wordmark colored", "devicon-javascript-plain colored", "devicon-jquery-plain-wordmark colored", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored"],
 		target: 'mti-target',
 		site: null,
 		github: null
