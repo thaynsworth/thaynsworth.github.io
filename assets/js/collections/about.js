cdae69a3db70ae333ef428d25b931163a0896ad5
thaynsworth <thomas.haynsworth@gmail.com>
2015-08-03 20:52:08 -0400

    added basic styling to about section

--- assets/js/collections/about.js
+++ assets/js/collections/about.js
@@ -5,7 +5,8 @@ var About = Backbone.Collection.extend({
 var data = [
 
 {
-	name: 'Tom Haynsworth',
+	name: 'Tom Haynsworth | Web Developer',
+	profileImage: 'assets/img/profile2.jpg',
 	techAbout: '',
 	workHistory: 'I’m a hotel management professional turned developer and have been coding/building for the last 11 months. My previous management experience has taught me the importance of communication, critical/innovative thinking, and good leadership. Combined with my technical coding skills and I know that I would be a great long-term asset to your company!'
 }
