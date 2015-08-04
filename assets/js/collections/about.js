e8e982bde455605a841ce87a1701b61c6a0fa7a9
thaynsworth <thomas.haynsworth@gmail.com>
2015-08-04 19:05:20 -0400

    fixed wrapper scroll

--- assets/js/collections/about.js
+++ assets/js/collections/about.js
@@ -8,7 +8,8 @@ var data = [
 	name: 'Tom Haynsworth | Web Developer',
 	profileImage: 'assets/img/profile2.jpg',
 	techAbout: '',
-	workHistory: 'I’m a hotel management professional turned developer and have been coding/building for the last 11 months. My previous management experience has taught me the importance of communication, critical/innovative thinking, and good leadership. Combined with my technical coding skills and I know that I would be a great long-term asset to your company!'
+	workHistory: 'I’m a hotel management professional turned developer and have been coding/building for the last 11 months. My previous management experience has taught me the importance of communication, critical/innovative thinking, and good leadership. Combined with my technical coding skills and I know that I would be a great long-term asset to your company!',
+	
 }
 
 ];
