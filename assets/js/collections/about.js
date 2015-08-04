d83d02c9d3b9675b4a587fa0bcc059a9101b61ad
thaynsworth <thomas.haynsworth@gmail.com>
2015-08-04 19:25:03 -0400

    updated section headers / profile format

--- assets/js/collections/about.js
+++ assets/js/collections/about.js
@@ -7,9 +7,9 @@ var data = [
 {
 	name: 'Tom Haynsworth | Web Developer',
 	profileImage: 'assets/img/profile2.jpg',
-	techAbout: '',
+	techAbout: 'As a Full Stack Developer I enjoy all aspects of the development process. ',
 	workHistory: 'I’m a hotel management professional turned developer and have been coding/building for the last 11 months. My previous management experience has taught me the importance of communication, critical/innovative thinking, and good leadership. Combined with my technical coding skills and I know that I would be a great long-term asset to your company!',
-	
+
 }
 
 ];
