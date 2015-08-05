02b274831087849ebc42e416921e3414fc8cfd66
thaynsworth <thomas.haynsworth@gmail.com>
2015-08-05 11:17:14 -0400

    styled stack icons / added buttons to about

--- assets/js/collections/about.js
+++ assets/js/collections/about.js
@@ -9,6 +9,10 @@ var data = [
 	profileImage: 'assets/img/profile2.jpg',
 	techAbout: 'As a Full Stack Developer I enjoy all aspects of the development process. ',
 	workHistory: 'I’m a hotel management professional turned developer and have been coding/building for the last 11 months. My previous management experience has taught me the importance of communication, critical/innovative thinking, and good leadership. Combined with my technical coding skills and I know that I would be a great long-term asset to your company!',
+	languages: ["devicon-html5-plain-wordmark", "devicon-javascript-plain", "devicon-ruby-plain-wordmark"],
+	frameworks: ["devicon-rails-plain-wordmark", "devicon-jquery-plain-wordmark", "devicon-backbonejs-plain-wordmark", "devicon-nodejs-plain-wordmark", "devicon-wordpress-plain-wordmark", "devicon-git-plain-wordmark", "devicon-meteor-plain-wordmark"],
+	database: ["devicon-postgresql-plain-wordmark", "devicon-mongodb-plain-wordmark"],
+	design: ["devicon-css3-plain-wordmark", "devicon-bootstrap-plain-wordmark", "devicon-sass-original", "devicon-photoshop-line"]
 
 }
 
