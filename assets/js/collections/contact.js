5f1a406b148bbf1eb382b36ba4bdb51bb6e2c566
thaynsworth <thomas.haynsworth@gmail.com>
2015-08-05 13:35:26 -0400

    added contact info/icons

--- assets/js/collections/contact.js
+++ assets/js/collections/contact.js
@@ -5,8 +5,17 @@ var Contact = Backbone.Collection.extend({
 var data = [
 
 {
-	description: 'Would love to hear from you!',
-	info: 'assets/img/profile2.jpg',
+	description: "I would love to hear from you! Please reach out to me for questions or comments",
+	contactInfo: 
+	[
+	"<span class='glyphicon glyphicon-envelope'></span> thomas.haynsworth@gmail.com",
+	"<span class='glyphicon glyphicon-phone'></span> 908.447.6537"
+	],
+	socialInfo:
+	[
+	"<i class='fa fa-github fa-3x'></i></i> https://github.com/thaynsworth",
+	"<i class='fa fa-linkedin fa-3x'></i> https://www.linkedin.com/in/thomashaynsworth"
+	]
 
 }
 
