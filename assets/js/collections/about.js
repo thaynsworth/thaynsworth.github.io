b18907d8070ff5c27ebf3dc036fab3cba2b0efa1
thaynsworth <thomas.haynsworth@gmail.com>
2015-08-06 10:59:17 -0400

    cleaned up old code

--- assets/js/collections/about.js
+++ assets/js/collections/about.js
@@ -9,7 +9,7 @@ var data = [
 	profileImage: "assets/img/profile2.jpg",
 	techAbout: "I'm a Freelance Web Developer that strives to push the limits on every project I undertake. As a Full Stack Developer I enjoy all aspects of the development process, from researching and designing through building and deployment. I'm constantly challenging myself to use new technologies and frameworks to enhance the user experience.",
 	workHistory: "I have a wide variety of professional work experience. Prior to Technology I spent 7+ years in Hospitality Management, leading multiple departments in a large resort atmosphere. My previous roles strengthened my communication, leadership, and critical/innovative thinking skills.",
-	contact: "If you're looking for a well rounded Web Developer, look no further! <a></a>",
+	contact: "If you're looking for a well-rounded Web Developer, look no further! <a></a>",
 	languages: ["devicon-html5-plain-wordmark", "devicon-javascript-plain", "devicon-ruby-plain-wordmark"],
 	frameworks: ["devicon-rails-plain-wordmark", "devicon-jquery-plain-wordmark", "devicon-backbonejs-plain-wordmark", "devicon-nodejs-plain-wordmark", "devicon-wordpress-plain-wordmark", "devicon-git-plain-wordmark", "devicon-meteor-plain-wordmark"],
 	database: ["devicon-postgresql-plain-wordmark", "devicon-mongodb-plain-wordmark"],
