e9f3438104869fd5ae45be648c8afedbd751b2d5
thaynsworth <thomas.haynsworth@gmail.com>
2015-08-07 12:16:22 -0400

    updated collection format

--- assets/js/collections/contact.js
+++ assets/js/collections/contact.js
@@ -6,15 +6,13 @@ var data = [
 
 {
 	description: "I would love to hear from you! I'm available for Freelance projects, Full/Part-time work, consulting, or anything really! :)",
-	contactInfo: 
-	[
-	"<a href='mailto:thomas.haynsworth@gmail.com'><i class='fa fa-envelope-o fa-3x'></i><span>thomas.haynsworth@gmail.com</span></a>",
-	"<a href='tel://1-908-447-6537'><i class='fa fa-mobile fa-3x'></i><span>908.447.6537</span></a>"
+	contactInfo: [
+		"<a href='mailto:thomas.haynsworth@gmail.com'><i class='fa fa-envelope-o fa-3x'></i><span>thomas.haynsworth@gmail.com</span></a>",
+		"<a href='tel://1-908-447-6537'><i class='fa fa-mobile fa-3x'></i><span>908.447.6537</span></a>"
 	],
-	socialInfo:
-	[
-	"<a href='https://github.com/thaynsworth'><i class='fa fa-github fa-3x'></i><span>https://github.com/thaynsworth</span></a>",
-	"<a href='https://www.linkedin.com/in/thomashaynsworth'><i class='fa fa-linkedin fa-3x'></i><span>https://www.linkedin.com/in/thomashaynsworth</span></a>"
+	socialInfo: [
+		"<a href='https://github.com/thaynsworth'><i class='fa fa-github fa-3x'></i><span>https://github.com/thaynsworth</span></a>",
+		"<a href='https://www.linkedin.com/in/thomashaynsworth'><i class='fa fa-linkedin fa-3x'></i><span>https://www.linkedin.com/in/thomashaynsworth</span></a>"
 	]
 
 }
